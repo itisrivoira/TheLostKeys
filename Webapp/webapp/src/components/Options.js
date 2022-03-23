67a29c87df056671e271f29cab11c7760af8ead7
MatteoSeimandi <matteoseimandi03@gmail.com>
2022-03-23 09:45:52 +0100

    fullscreen enabled

--- Webapp/webapp/src/components/Options.js
+++ Webapp/webapp/src/components/Options.js
@@ -1,15 +1,12 @@
 // Pannello delle Opzioni
-import { useState } from "react";
-import { Button, Modal } from "react-bootstrap";
+
+import { Col, Button, Modal, Row } from "react-bootstrap";
+import useFullScreen from "../utils/useFullScreen";
 
 import '../style/App.css';
 
 const Options = props => {
-	const [full, setFull] = useState(false);
-	// vero --> schermo intero attivo
-	// falso --> schermo intero spento
-	// https://github.com/Darth-Knoppix/example-react-fullscreen/blob/master/src/utils/useFullscreenStatus.js
-	// da modificare
+	const { fullScreen, toggle } = useFullScreen();
 
 	return(
 		<Modal
@@ -20,7 +17,7 @@ const Options = props => {
 		>
 			<Modal.Header
 				closeButton
-				className="bg-secondary"
+				className="bg-secondary border border-dark border-4"
 			>
 				<Modal.Title className="w-100">
 					<p className="txt-pixel text-center fs-2 m-0">
@@ -29,13 +26,21 @@ const Options = props => {
 				</Modal.Title>
 			</Modal.Header>
 			<Modal.Body className="bg-secondary">
-				<p>
-					Queste sono le opzioni
-				</p>
+				<Row className="text-center">
+					<Col>
+						<p>
+							Queste sono le opzioni
+						</p>
+					</Col>
+				</Row>
+				<Row className="text-center">
+					<Col>
+						<Button onClick={toggle} size='lg' variant="dark" >
+							{fullScreen ? 'Exit FullScreen' : 'Toggle FullScreen'}
+						</Button>
+					</Col>
+				</Row>
 
-				<Button variant="outline-secondary">
-					Toggle FullScreen
-				</Button>
 			</Modal.Body>
 		</Modal>
 	)
