94c02400a47c0f16220393ef9f42ed3a0880ddf7
666mandi <58552446+MatteoSeimandi@users.noreply.github.com>
2022-03-26 11:21:48 +0100

    Aggiunto Navigatore Web

--- Webapp/webapp/src/index.js
+++ Webapp/webapp/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
