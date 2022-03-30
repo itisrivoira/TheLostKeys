8fafa68d31a8bc8c4226f413d391dc4295bfe698
MatteoSeimandi <matteoseimandi03@gmail.com>
2022-03-30 09:28:32 +0200

    Disabilitazione tasto F11

--- Webapp/webapp/src/App.js
+++ Webapp/webapp/src/App.js
@@ -1,11 +1,21 @@
 // Componente root della Web App
 
+import { useCallback, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import Menu from "./pages/Menu";
 import Play from "./pages/Play";
 
 const App = () => {
+
+	const prevent = useCallback(e => {
+		if (e.key == 'F11')
+			e.preventDefault();
+	})
+
+	useEffect( () => {
+		document.addEventListener('keydown', prevent, false);
+	}, []);
 
 	return (
 		<Routes>
