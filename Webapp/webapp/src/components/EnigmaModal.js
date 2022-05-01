f1f7a15e747f4e4bd036ce2e0adc6ddd23a1ee57
666mandi <58552446+MatteoSeimandi@users.noreply.github.com>
2022-05-01 16:53:10 +0200

    Preparazione Modifiche Enigmi

--- Webapp/webapp/src/components/EnigmaModal.js
+++ Webapp/webapp/src/components/EnigmaModal.js
@@ -1,85 +1,64 @@
 // Schermata Enigma
 
-import { useContext } from "react";
-import { Modal, Button, Row, Col } from "react-bootstrap";
+import { useContext, useState } from "react";
+import { Modal, Row, Col } from "react-bootstrap";
 
 import { Enigma } from './components';
+import puzzles from '../assets/puzzles/puzzles.json';
 
 import '../style/enigma.css';
 
 const EnimaModal = () => {
 	const { enigma, setEnigma } = useContext(Enigma);
+	const [tentativi, setTentativi] = useState(3);
+	const [disable1, setDisable1] = useState(false);
+	const [disable2, setDisable2] = useState(false);
+	const [disable3, setDisable3] = useState(false);
+	const [disable4, setDisable4] = useState(false);
 
-	const giusto = () => {
-		alert('giusto :)');
-		setEnigma(false);
+	const close = () => setEnigma(false);
+	const right = () => alert('giusto :)');
+	const wrongs = () => {
+		alert('sbagliato :(');
+		setTentativi( prev => prev - 1 );
 	};
 
-	const sbagliato = () => {
-		alert('sbagliato :(');
-		setEnigma(false);
-	}
+	const check = () => {
+	};
 
 	return(
 		<Modal
 			show={enigma}
-			onHide={() => setEnigma(false)}
+			onHide={close}
 			animation={false}
-			keyboard
-			onEscapeKeyDown={e => e.preventDefault()}
 			fullscreen
-
 		>
 			<Modal.Body className="back">
 				<Row>
 					<Col>
-						<p className="fs-3 txt-pixel">
-							Ecco qui l'enigma <br/>
-							Risolvi lo schema per il passaggio di stato: da una sostanza solida
-							come il ghiaccio qual è il giusto ordine per passare allo stato
-							gassoso, e poi a quello liquido come l’acqua e poi di nuovo allo
-							stato solido
+						<p>
+							N° Tentativi Rimasti: {tentativi}
 						</p>
 					</Col>
 				</Row>
-				<hr />
-				<Button variant="success" onClick={sbagliato} className='my-2'>
-					<p className="txt-pixel fs-4">
-						Liquefazione - Sublimazione - Solidificazione
-					</p>
-				</Button>
-				<br/>
-				<Button variant="success" onClick={sbagliato} className='my-2'>
-					<p className="txt-pixel fs-4">
-						Non si può determinare
-					</p>
-				</Button>
-				<br/>
-				<Button variant="success" onClick={giusto} className='my-2'>
-					<p className="txt-pixel fs-4">
-						Sublimazione - Liquefazione - Solidificazione
-					</p>
-
-				</Button>
-				<br/>
-				<Button variant="success" onClick={sbagliato} className='my-2'>
-					<p className="txt-pixel fs-4">
-							Sublimazione - Solidificazion - Liquefazione
-					</p>
-
-				</Button>
-				<br/>
-				<hr />
-				<Button variant="danger" onClick={() => setEnigma(false)}>
-					<p className="txt-pixel fs-3">
-						Rinuncia
-					</p>
-				</Button>
-				<Button variant="warning" className="mx-3">
-					<p className="txt-pixel fs-3">
-						Suggerimento
-					</p>
-				</Button>
+				<Row className="">
+					<Col>
+						Immagine
+					</Col>
+					<Col>
+						Risposte
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<p>
+							{puzzles.Chimica.text}
+						</p>
+					</Col>
+					<Col>
+						Bottoni
+					</Col>
+				</Row>
 			</Modal.Body>
 		</Modal>
 	)
