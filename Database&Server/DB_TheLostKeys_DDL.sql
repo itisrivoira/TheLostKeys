8a73f23b89ec4dd8f4c19efa05a007889be9f265
aleksandra <alek.venezia@hotmail.com>
2022-05-18 17:13:31 +0200

    Update DB_TheLostKeys_DDL.sql

--- Database&Server/DB_TheLostKeys_DDL.sql
+++ Database&Server/DB_TheLostKeys_DDL.sql
@@ -5,15 +5,14 @@ USE TheLostKeys;
 CREATE TABLE IF NOT EXISTS utente(
 	Nickname varchar(25) NOT NULL,
 	Password varchar(32),
-	Email varchar(70) ,
+	Email varchar(70),
 	PRIMARY KEY (Nickname)
 	);
 
 CREATE TABLE IF NOT EXISTS partita(
 	Id INT  NOT NULL AUTO_INCREMENT,
-	Tempo TIME NOT NULL,
-	PunteggioMassimo INT NOT NULL,
-	Giocatore varchar(25),
+	Punteggio INT NOT NULL,
+	Personaggio varchar(25),
 	Nick varchar(25),
 	PRIMARY KEY (Id),
 	FOREIGN KEY (Nick) REFERENCES utente(Nickname)
