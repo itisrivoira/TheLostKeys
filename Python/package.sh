03360da9138b6b9ffb4070f10bfdd2ed98f8dee5
SenestroStefano <senestrostefano003@gmail.com>
2022-05-23 14:53:15 +0200

    fix

--- Python/package.sh
+++ Python/package.sh
@@ -4,6 +4,8 @@
 
 pip3 install pygame
 pip3 install pygame --version
+sudo apt-get update -y
 pip3 install pandas
 pip3 install ffpyplayer
 pip3 install pymediainfo
+sudo apt-get install -y libmediainfo-dev
